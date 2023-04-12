fa772a18bcf62e5aeae5b5a292761d621b7a48f5
Thomas Numnum <dreadpirateroberts1350@protonmail.com>
2023-04-12 16:30:12 -0500

    edited login and signup

--- client/resources/scripts/signup.js
+++ client/resources/scripts/signup.js
@@ -1,33 +1,40 @@
+// SIGNUP.JS
+
+const userUrl = "https://your-api-endpoint.com/users"; // Replace with the correct API endpoint for creating a user
+
 function userSignUp() {
     const signUpForm = document.getElementById("signup-form");
     const loginButton = document.getElementById("login-director");
     
     signUpForm.addEventListener("submit", createUser);
     loginButton.addEventListener("click", () => {
-      console.log("User signed up");
-      window.location.href = "../pages/index.html"; // Redirect to the main page
+      window.location.href = "../pages/login.html"; // Redirect to the login page
     });
 }
 
 ///// CREATE USER /////
 const createUser = async (event) => {
-    event.preventDefault();
-    const target = event.target;
-    const user = {
-      email: target.email.value,
-      password: target.password.value,
-    };
-    await fetch(url, {
-      method: "POST",
+  event.preventDefault();
+  const target = event.target;
+  const user = {
+      Email: target["email-input-signup"].value,
+      Password: target["password-input-signup"].value
+  }
+  const response = await fetch(userUrl, {
+      method: 'POST',
       headers: {
-        Accept: "*/*",
-        "Content-Type": "application/json",
+        Accept: '*/*',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(user),
-    });
-    render();
-    target.email.value = "";
-    target.password.value = "";
-};
+  });
+
+  if (response.ok) {
+      console.log("User created successfully");
+      window.location.href = "../pages/login.html"; // Redirect to the login page
+  } else {
+      console.error("Error creating user:", response.statusText);
+  }
+}
   
 userSignUp();
